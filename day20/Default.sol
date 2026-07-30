846800b683cc9b23c4ded9b3f91b6201723b8389
Mohammed Soliman <mohammedsolly4@gmail.com>
2026-07-30 22:25:06 +0300

    Day 20: Default values - what uninitialized state variables default to

---
+++ day20/Default.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract DEFAULT {
+    // default values
+
+    uint public myUint; // = 0
+    int public myInt; // = 0
+
+    bool public myBool; // = false 
+
+    address public myAddress; // 0x000000000000000000000000000
+    
+    bytes32 public  myBytes; // 
+
+}
