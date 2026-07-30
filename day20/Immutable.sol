d80ee318a3fd7d77514e0f4f87e4821abc704047
Mohammed Soliman <mohammedsolly4@gmail.com>
2026-07-30 22:25:22 +0300

    Day 20: Immutable - set once in constructor, cheaper than storage reads

---
+++ day20/Immutable.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+contract IMMUTABLE {
+  // the immutable  declared and  then be initialize it in the constructor 
+
+  // declration
+address immutable owner;
+
+  // initialize
+
+  constructor(address _owner){
+
+    owner = _owner;
+  }
+
+
+    
+}
+
