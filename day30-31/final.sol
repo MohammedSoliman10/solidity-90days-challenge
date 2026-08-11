028224d9b7a537c98f9e5a3f0e4c19a067f10888
Mohammed Soliman <mohammedsolly4@gmail.com>
2026-08-11 17:42:26 +0300

    Day 30-31: the complete version

--- day30-31/final.sol
+++ day30-31/final.sol
@@ -16,6 +16,7 @@ contract Freelancing {
     }
 
     // constants & state vars
+    uint256 constant REPUTATION_REWARD = 1;
     uint256 public listingFee = 0.001 ether;
     address public owner;
     uint256 public totalJobs;
@@ -26,6 +27,7 @@ contract Freelancing {
 
     // events
     event JobCreated(uint256 indexed jobId, address indexed client, uint256 bounty);
+    event JobTaken(uint256 indexed jobId, address indexed freelancer);
     event JobCompleted(uint256 indexed jobId, address indexed freelancer);
 
     // errors
@@ -41,4 +43,58 @@ contract Freelancing {
     constructor() {
         owner = msg.sender;
     }
+
+    function postJob(string calldata _title, string calldata _description) external payable {
+        if (msg.value <= listingFee){
+            revert InsufficientFunds();
+        }
+        totalJobs++;  //job id
+        jobs[totalJobs] = Job({
+            title: _title,
+            description: _description,
+            bounty: msg.value - listingFee,
+            client: msg.sender,
+            freelancer: address(0),
+            status: JobStatus.Open    
+        });
+        emit JobCreated(totalJobs, msg.sender, msg.value - listingFee);
+
+    }
+
+    function getAllJobs() external view returns (Job[] memory) {
+        Job[] memory allJobs = new Job[](totalJobs);
+        for (uint256 i = 1; i <= totalJobs; i++) {
+            allJobs[i - 1] = jobs[i];
+        }
+        return allJobs;
+    }
+
+    function acceptJob(uint256 _jobId) external {
+        Job storage job = jobs[_jobId];
+        require(job.status == JobStatus.Open, "Job not available");
+        require(job.client != msg.sender, "Client cannot accept their own job");
+        job.freelancer = msg.sender;
+        job.status = JobStatus.Taken;
+        emit JobTaken(_jobId, msg.sender);
+    }
+
+    function completeJob(uint256 _jobId) external {
+        Job storage job = jobs[_jobId];
+        require(job.status == JobStatus.Taken, "Job not taken");
+        require(msg.sender == job.client, "Only client can mark job as completed");
+        job.status = JobStatus.Completed;
+        freelancerReputation[job.freelancer] += REPUTATION_REWARD;
+        emit JobCompleted(_jobId, msg.sender);
+        (bool success, ) = payable(job.freelancer).call{value: job.bounty}(""); 
+        require(success, "Transfer failed");
+    }
+
+    function setListingFee(uint256 _newFee) external onlyOwner {
+        listingFee = _newFee;
+    }
+    function getPlatformBalance() external view onlyOwner returns (uint256) {
+        return address(this).balance;
+    }
+    
+
 }
